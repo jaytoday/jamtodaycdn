865f0ef02856bf0eecd09ade085c5539a431c700
James <james@jamess-macbook-air.local>
2009-08-10 19:22:33 -0700

    added memoize fxn

--- frontend.py
+++ frontend.py
@@ -11,7 +11,8 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 from models import Image
-  
+from memoize import memoize
+
 class GenericServer(webapp.RequestHandler):
     """
     Image server designed to handle serving png images from
@@ -20,18 +21,23 @@ class GenericServer(webapp.RequestHandler):
     property = 'image'
     def get(self):
         # key is provided in the query string
-        image = db.get(self.request.get("id"))
-        if image.image:
+        image = get_image(self.request.get("id"), self.property)
+        if image:
             # we have an image so prepare the response
             # with the relevant headers
             self.response.headers['Content-Type'] = "image/png"
             # and then write our the image data direct to the response
-            self.response.out.write(eval("image.%s" % self.property))
+            self.response.out.write(image)
         else:
             # we should probably return an image with the correct header
             # here instead of the default html 404
             self.error(404)
 
+@memoize()
+def get_image(image_id, property):
+  image = db.get(image_id)
+  return eval("image.%s" % property)
+  
 class ImageServer(GenericServer):
     "Serve the main image"
     property = 'image'

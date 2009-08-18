e89458cd65818bb0a52af68d2b907f63b956c7ce
james <james@james-desktop.(none)>
2009-08-18 16:21:17 -0700

    syntax highlighter

--- assets/code_highlighter/python.js
+++ assets/code_highlighter/python.js
@@ -2,8 +2,8 @@ CodeHighlighter.addStyle("python",{
 	comment : {
 		exp  : /#[^\n]+/
 	},
-	brackets : {
-		exp  : /(*|+|-|=|.)/
+	operators : {
+		exp  : /(-|\*|\+|=|\.|:|\(|\)|\{|\})/
 	},
 	string : {
 		exp  : /'[^']*'|"[^"]*"/
@@ -11,7 +11,7 @@ CodeHighlighter.addStyle("python",{
 	keywords : {
 		exp  : /\b(do|end|if|module|yield|then|else|for|until|unless|while|elsif|case|when|break|retry|redo|rescue|require|raise)\b/
 	},
-	symbol : {
+	constants : {
 	  exp : /\b(self|class|def)\b/
 	}
 });
